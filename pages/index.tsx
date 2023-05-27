25b444546d6780b57d87b7312d74a943a67812e3
Leon2192 <leon.strokes@gmail.com>
2023-05-27 15:20:50 -0300

    deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,48 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage, NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+import useCurrentUser from "../hooks/useCurrentUser";
+import Navbar from "../components/Navbar";
+import Billboard from "../components/Billboard";
+import MovieList from "../components/MovieList";
+import useMovieList from "../hooks/useMovieList";
+import useFavorites from "../hooks/useFavorites";
+import InfoModal from "../components/InfoModal";
+import useInfoModal from "../hooks/useInfoModal";
 
-const Home: NextPage = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">Netflix</h1>
-  )
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 }
 
-export default Home
+const Home: NextPage = () => {
+  //const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
+  const {isOpen, closeModal} = useInfoModal();
+
+  return (
+    <div>
+      <InfoModal visible={isOpen} onClose={closeModal} />
+      <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending now" data={movies} />
+        <MovieList title="My List" data={favorites} />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
